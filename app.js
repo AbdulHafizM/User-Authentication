481f73ba1323c698961df2f9495e1e1a2b700d07
AbdulHafizM <muibiabdulhafiz007@gmail.com>
2023-08-24 23:25:13 +0100

    almost there

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 const express = require('express')
 const session = require('express-session')
 const connectDB = require('./db/connect')
+const authRouter = require('./routes/auth')
+const path = require('path')
 const MongoDBSession = require('connect-mongodb-session')(session)
 require('dotenv').config()
 const app = express()
 
-app.use(express.static('./public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
-
-const store = new MongoDBSession({
-    uri: process.env.MONGO_URI,
-    databaseName: 'Sessions',
-    collection: 'mySessions'
-})
-
-app.use(session({
-    secret: 'Some key signing cookie',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-}))
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello')
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 const start = async() => {
     try{
@@ -39,5 +26,40 @@ const start = async() => {
 
 start()
 
+const store = new MongoDBSession({
+    uri: process.env.MONGO_URI,
+    collection: 'mySessions',
+})
+
+app.use(session({
+    secret: 'Some key signing cookie',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+}))
+
+app.use('', authRouter)
+
+const authenticate = (req,res,next) => {
+    if(req.session.isAuth){
+        next()
+    }
+    res.redirect('/login')
+}
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
 
 
+app.get('/login',(req,res)=>{
+    res.render('pages/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('pages/register')
+})
+
+app.get('/dashboard',authenticate,(req,res)=>{
+    res.render('pages/dashboard')
+})
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,25 @@
-const userModel = require('./models/user')
+const User = require('../models/user')
 const bcrypt = require('bcrypt')
+
+const login = async(req,res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+    if(!user){
+        return res.redirect('/login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+        return res.redirect('/login')
+    }
+    req.session.isAuth = true
+    res.redirect('/dashboard')
+}
+
 
 const register = async(req,res) => {
     const {username, email, password} = req.body
-    let user = await userModel.findOne({email})
+    const user = await User.findOne({email})
 
     if(user){
         return res.redirect('/register')
@@ -11,10 +27,20 @@ const register = async(req,res) => {
 
     const hashpass = await bcrypt.hash(password, 10)
 
-    user = new userModel({
+    const userm = new User({
         username: username,
         email: email,
         password: hashpass
     })
-    await user.save()
+    await userm.save()
+    res.redirect('/login')
 }  
+
+const logout = (req,res) => {
+    req.session.destroy((err)=>{
+        if(err) throw err;
+        res.redirect('/login')
+    })
+}
+
+module.exports = {login, register, logout}
